96cba08889f20b311e9c34e56126030c63760c2f
pranay <mangal.pranay@gmail.com>
2022-10-23 12:59:58 +0530

    note model, routes created

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+//available routes
+ 
+app.use('/api/auth',require('./routes/auth'))
+
+app.use('/api/notes',require('./routes/notes'))
+
 app.get('/', (req, res) => {
-  res.send('Hello pranay!')
-})
+    res.send('Hello pranay!')
+  })
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost: ${port}`)
 })
 
  
